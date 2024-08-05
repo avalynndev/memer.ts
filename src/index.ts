ac9eecc80250f6a10fc18f52569ee34bc5ca2eb3
avalynn <100826194+avalynndev@users.noreply.github.com>
2024-08-05 13:46:56 +0000

    added github repository and use other canvas

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { createCanvas, loadImage } from "canvas";
+import { createCanvas, loadImage } from "@napi-rs/canvas";
 
 export async function disability(avatar: string): Promise<Buffer> {
   try {
