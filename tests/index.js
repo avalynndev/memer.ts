f17888c18143c57e4919e3fea753d26901953743
avalynn <100826194+avalynndev@users.noreply.github.com>
2024-08-09 15:36:09 +0000

    tests

--- tests/index.js
+++ tests/index.js
@@ -1,11 +1,11 @@
 const fs = require("fs").promises;
-const { disability } = require( "memer.ts");
+const { jail } = require( "memer.ts");
 
 async function createImageFile() {
 	try {
 		const avatarUrl =
 			"https://cdn.discordapp.com/avatars/887939910177275905/816909b74b25a66ec455e5143ff8e8ff.png";
-		const buffer = await disability(avatarUrl);
+		const buffer = await jail(avatarUrl);
 
 		// Write the buffer to a file called image.png
 		await fs.writeFile("image.png", buffer);
