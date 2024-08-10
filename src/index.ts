f1805ca6253c3bfbffdb7e5c8fa314f876ea9a04
Mohtasim Alam Sohom <94981761+devaoto@users.noreply.github.com>
2024-08-10 12:32:51 +0600

    Add Memer class

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,38 @@
-export * from "./functions";
+import {
+	disability,
+	hitler,
+	humanity,
+	egg,
+	excuseme,
+	dab,
+	door,
+	cry,
+	failure,
+	idelete,
+	jail,
+	roblox,
+	satan,
+	trash,
+	youtube,
+} from "./functions";
+
+class Memer {
+	public disability = disability;
+	public dab = dab;
+	public door = door;
+	public cry = cry;
+	public egg = egg;
+	public excuseme = excuseme;
+	public failure = failure;
+	public hitler = hitler;
+	public humanity = humanity;
+	public idelete = idelete;
+	public jail = jail;
+	public roblox = roblox;
+	public satan = satan;
+	public trash = trash;
+	public youtube = youtube;
+}
+
+export { Memer };
 export * from "./utils";
